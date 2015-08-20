9d650d8fc4d8c4b0c13b7f9a8bb740f372345d27
Philipp Dittert <p.dittert@techdivision.com>
2015-08-20 13:33:07 +0200

    initial commit

---
+++ Classes/TechDivision/ModuleBase/Controller/Module/TechDivisionController.php
@@ -0,0 +1,24 @@
+<?php
+namespace TechDivision\ModuleBase\Controller\Module;
+
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to version 3 of the GPL license,
+ * that is bundled with this package in the file LICENSE, and is
+ * available online at http://www.gnu.org/licenses/gpl.txt
+
+ * @author    Philipp Dittert <pd@techdivision.com>
+ * @copyright 2015 TechDivision GmbH <info@techdivision.com>
+ * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public License, version 3 (GPL-3.0)
+ * @link      https://github.com/techdivision/TechDivision.ModuleBase
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class TechDivisionController extends \TYPO3\Neos\Controller\Module\AbstractModuleController
+{
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+
+# Introduction
+
+This Package provides the TechDivision backend module section.
+
+
+# Installation
+
+you need to add the package to your `composer.json`
+
+``` bash
+{
+    "require": {
+        "techdivision/modulebase": "1.0.*"
+    },
+}
+```
+
+Install the package:
+
+``` bash
+composer update techdivision/modulebase
+```
